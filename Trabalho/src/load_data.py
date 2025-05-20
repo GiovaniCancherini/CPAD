4e4b121003d383a45e9e1af62e75376d61c9d8d1
Giovani Cancherini <giovani.canquerini@gmail.com>
2025-05-19 23:05:53 -0300

    feat: on the way handwork

--- Trabalho/src/load_data.py
+++ Trabalho/src/load_data.py
@@ -1,11 +1,21 @@
-import pandas as pd
 import os
+import pandas as pd
 
-def load_all_data(path='data/'):
+def load_all_data(path=None):
+    if path is None:
+        # Caminho absoluto da pasta data, a partir do src
+        path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data'))
+
     dfs = []
     for year in range(2017, 2021):
-        file_path = os.path.join(path, f'prouni_{year}.csv')
-        df = pd.read_csv(file_path, sep=';', encoding='utf-8')
-        df['ANO_CONCESSAO_BOLSA'] = year  # garantir consistencia
+        file_path = os.path.join(path, f'pda-prouni-{year}.csv')
+        
+        try:
+            df = pd.read_csv(file_path, sep=';', encoding='utf-8-sig')
+        except UnicodeDecodeError:
+            df = pd.read_csv(file_path, sep=';', encoding='latin1')
+            
+        df['ANO_CONCESSAO_BOLSA'] = year
         dfs.append(df)
+
     return pd.concat(dfs, ignore_index=True)
